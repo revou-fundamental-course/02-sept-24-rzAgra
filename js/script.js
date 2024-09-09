ec4684950a45b2b817af19ea7cc95e5cb67f9242
RzAgra <razzanagra@gmail.com>
2024-09-09 22:06:49 +0700

    update 09sept

--- js/script.js
+++ js/script.js
@@ -1,17 +1,12 @@
 function hitung() {
-    // Ambil nilai alas dan tinggi
     var alas = document.getElementById("alas").value;
     var tinggi = document.getElementById("tinggi").value;
 
-    // Pastikan alas dan tinggi bukan kosong
     if(alas && tinggi) {
-        // Rumus luas segitiga = 1/2 * alas * tinggi
         var hasil = 0.5 * alas * tinggi;
-
-        // Tampilkan hasil
         document.getElementById("hasil").value = hasil;
     } else {
-        alert("Mohon isi nilai alas dan tinggi");
+        alert("Input masih kosong!!!");
     }
 }
 
@@ -24,6 +19,6 @@ function hitungKeliling() {
         var hasilKeliling = parseFloat(sisiA) + parseFloat(sisiB) + parseFloat(sisiC);
         document.getElementById("hasil-keliling").value = hasilKeliling;
     } else {
-        alert("Mohon isi semua nilai sisi");
+        alert("Input Masih kosong!!!");
     }
 }
